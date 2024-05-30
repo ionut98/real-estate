196224e2a8a9803fbec53418ce1623ee624997c7
ionut-mihoci <ionut.mihoci@fortechproducts.ro>
2024-05-30 13:39:50 +0300

    header section is done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className="bg-gray-100 shadow-sm">
-      <div className="flex justify-between items-center w-full h-11 p-2">
-        <h1 className="font-bold text-2xl flex flex-wrap">
-          <span className="text-yellow-600">Realm</span>
-          <span className="text-gray-700">Estate</span>
-        </h1>
-        <form>
+    <header className="bg-blue-50 shadow-sm">
+      <div className="flex justify-between items-center w-full p-2">
+        <Link to={'/'}>
+          <h1 className="font-bold text-2xl flex flex-wrap">
+            <span className="text-blue-700">Realm</span>
+            <span className="text-gray-700">Estate</span>
+          </h1>
+        </Link>
+
+        <form className="bg-white p-3 rounded-lg flex flex-wrap items-center">
           <input
             type="text"
-            className="bg-transparent border-2 rounded-md p-1"
+            className="bg-transparent mr-1 focus:outline-none w-32 sm:w-64 md:w-72 lg:w-96"
             placeholder="Search"
           />
+          <FaSearch className="text-gray-700" />
         </form>
+
+        <ul className="flex gap-4">
+          <Link to={'/'}>
+            <li className="text-gray-700 font-semibold hidden sm:inline hover:text-blue-700 hover:cursor-pointer">
+              Home
+            </li>
+          </Link>
+          <Link to={'/about'}>
+            <li className="text-gray-700 font-semibold hidden sm:inline hover:text-blue-700 hover:cursor-pointer">
+              About
+            </li>
+          </Link>
+          <Link to={'/login'}>
+            <li className="text-gray-700 font-semibold sm:inline hover:text-blue-700 hover:cursor-pointer">
+              Login
+            </li>
+          </Link>
+        </ul>
       </div>
     </header>
   );
