b26d455b5269481bf39d84f000bdc700753af691
ionut-mihoci <ionut.mihoci@fortechproducts.ro>
2024-05-30 17:24:09 +0300

    connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,9 +1,22 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to Mongo", err);
+  });
+
 app.listen(30401, () => {
-  console.log('Server is listening on 30401');
+  console.log("Server is listening on 30401");
 });
 
 // app.get('/', (req, res) => {
