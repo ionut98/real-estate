8b719e190183a258cb81ce91ff3f0c2fecc36aec
ionut-mihoci <ionut.mihoci@fortechproducts.ro>
2024-05-31 18:04:49 +0300

    created login api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   console.log(req.body);
@@ -13,6 +15,34 @@ export const signup = async (req, res, next) => {
     await newUser.save();
 
     res.status(201).json("User created successfully!");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const foundUser = await User.findOne({ email });
+    if (!foundUser) {
+      return next(errorHandler(404, "User not found!"));
+    }
+
+    console.log(foundUser);
+
+    const isValidPassword = bcryptjs.compareSync(password, foundUser.password);
+    if (!isValidPassword) {
+      return next(errorHandler(401, "Wrong credentials!"));
+    }
+
+    const token = jwt.sign({ id: foundUser._id }, process.env.JWT_SECRET);
+    const { password: userPass, ...userPublicData } = foundUser._doc;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(userPublicData);
   } catch (err) {
     next(err);
   }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { login, signup } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
 authRouter.post("/signup", signup);
+authRouter.post("/login", login);
 
 export default authRouter;
