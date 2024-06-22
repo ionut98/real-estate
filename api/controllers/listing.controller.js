ebc609c88a7cb1954669b6443b604d61400365eb
iQuestUniversitymic <ionutmihoci@yahoo.com>
2024-06-22 18:42:00 +0300

    update listing wip

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -18,12 +18,35 @@ export const remove = async (req, res, next) => {
   }
 
   if (req.user.id !== listing.userRef) {
-    return next(errorHandler(404, "You can only delete your own listings!"));
+    return next(errorHandler(401, "You can only delete your own listings!"));
   }
 
   try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json("Listing deleted successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const update = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listings!"));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { add, remove } from "../controllers/listing.controller.js";
+import { add, remove, update } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const listingRouter = Router();
 
 listingRouter.post("/add", verifyToken, add);
 listingRouter.delete("/delete/:id", verifyToken, remove);
+listingRouter.post("/update/:id", verifyToken, update);
 
 export default listingRouter;
