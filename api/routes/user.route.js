84a61993c5c00ed4349e392a8af117f70ac6f268
iQuestUniversitymic <ionutmihoci@yahoo.com>
2024-06-22 16:35:34 +0300

    added get listing route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { updateUser, deleteUser } from "../controllers/user.controller.js";
+import {
+  updateUser,
+  deleteUser,
+  getUserListings,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const userRouter = Router();
 
 userRouter.post("/update/:id", verifyToken, updateUser);
 userRouter.delete("/delete/:id", verifyToken, deleteUser);
+userRouter.get("/listings/:id", verifyToken, getUserListings);
 
 export default userRouter;
