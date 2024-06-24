df7373548a53a30ffe922385bfca9cdd253095ac
iQuestUniversitymic <ionutmihoci@yahoo.com>
2024-06-24 20:41:48 +0300

    preinstall config

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
@@ -23,6 +24,8 @@ mongoose
     console.error("Failed to connect to Mongo", err);
   });
 
+const __dirname = path.resolve();
+
 app.listen(30401, () => {
   console.log("Server is listening on 30401");
 });
@@ -30,6 +33,12 @@ app.listen(30401, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
