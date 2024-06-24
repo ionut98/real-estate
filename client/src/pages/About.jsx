073dbea5987fc9703ef49da1a3d4da64cf16bf26
iQuestUniversitymic <ionutmihoci@yahoo.com>
2024-06-24 20:29:26 +0300

    added about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
 
 export default function About() {
-  return <div>About page</div>;
+  return (
+    <main className="py-20 px-3 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-gray-700">
+        About Realm Estate
+      </h1>
+      <p className="text-gray-700 mb-4">
+        Realm Estate is a premier real estate agency committed to providing
+        exceptional service and expertise to our clients. With years of
+        experience in the industry, we specialize in helping buyers, sellers,
+        and renters navigate the complex real estate market with ease. Our team
+        of dedicated professionals is passionate about delivering personalized
+        solutions tailored to meet the unique needs of each client, ensuring a
+        seamless and stress-free experience from start to finish.
+      </p>
+      <p className="text-gray-700 mb-4">
+        At Realm Estate, we pride ourselves on our deep knowledge of the local
+        market and our ability to offer a comprehensive range of services.
+        Whether you are looking to purchase your first home, invest in a
+        property, or find the perfect rental, our agents are equipped with the
+        tools and insights needed to achieve your goals. We leverage the latest
+        technology and marketing strategies to ensure your property gets maximum
+        exposure, and our negotiation skills are second to none, securing the
+        best possible outcomes for our clients.
+      </p>
+      <p className="text-gray-700 mb-4">
+        Our commitment to excellence goes beyond transactions; at Realm Estate,
+        we build lasting relationships based on trust and transparency. We
+        understand that real estate decisions are significant life events, and
+        we are dedicated to guiding you every step of the way. Choose Realm
+        Estate for a real estate partner who truly cares about your success and
+        satisfaction.
+      </p>
+    </main>
+  );
 }
